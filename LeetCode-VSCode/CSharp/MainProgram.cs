052fde6a3ceac88af1a224113cae833dfaa0d6b1
WangJun <jaxsonwang@foxmail.com>
2021-12-11 20:33:29 +0800

    feat(all): fix

--- LeetCode-VSCode/CSharp/MainProgram.cs
+++ LeetCode-VSCode/CSharp/MainProgram.cs
@@ -10,11 +10,16 @@ namespace LeetCodeExercise
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
-            StartCoroutine()
-            
+            List<int> A = new List<int>() { 2, 1, 0 };
+            List<int> B = new List<int>();
+            List<int> C = new List<int>();
+            // A.Add(2);
+            // A.Add(1);
+            // A.Add(0);
+            StringBuilder stringBuilder = new StringBuilder("123");
+            stringBuilder.Append("456");
+            Console.WriteLine(stringBuilder.ToString());
+            HanoiTower.Hanoi(A, B, C);
         }
-
-        
     }
 }
