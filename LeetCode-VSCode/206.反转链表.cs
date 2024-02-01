32f72cdfc809287d2331c2925944c6b86db6e0fa
Wang Jun <jaxsonwang@foxmail.com>
2024-02-01 23:42:06 +0800

    commit

--- LeetCode-VSCode/206.反转链表.cs
+++ LeetCode-VSCode/206.反转链表.cs
@@ -70,6 +70,19 @@
  */
 public class Solution
 {
+    // public ListNode ReverseList(ListNode head)
+    // {
+    //     if (head == null || head.next == null)
+    //     {
+    //         return head;
+    //     }
+    //     ListNode newHead = ReverseList(head.next);
+    //     head.next.next = head;
+    //     head.next = null;
+
+    //     return newHead;
+    // }
+    //反转链表 递归
     public ListNode ReverseList(ListNode head)
     {
         if (head == null || head.next == null)
